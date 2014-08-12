93bc71373806acc32a86e33cad1f71d78c0c02d9
Niall Jackson <niall.jackson@gmail.com>
2014-08-12 15:26:31 +0100

    FIle opener

--- ConfigurationalTemperature/analysis/C++/analyse.cpp
+++ ConfigurationalTemperature/analysis/C++/analyse.cpp
@@ -1,1 +1,23 @@
-#include "TrajReader.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "trajectory.h"
+
+int main(int argc, char** argv) {
+	if(argc < 2) {
+		std::cerr << "Usage: " << argv[0] << " <LAMMPS trajectory"
+			<< "filename>" << std::endl;
+		return 1;
+	}
+
+	std::string traj_filename(argv[1]);
+	Trajectory t(traj_filename);
+
+	if(!t.fail) {
+		std::cerr << "Couldn't open " << traj_filename
+			<< " for reading." << std::endl;
+		return 1;
+	}
+
+	return 0;
+}
--- ConfigurationalTemperature/analysis/TrajReader/TrajReader.h
+++ ConfigurationalTemperature/analysis/TrajReader/TrajReader.h
@@ -1,5 +1,10 @@
+#ifndef TRAJREADER_H
+#define TRAJREADER_H
+
 extern "C" {
 	bool openfile(char *, int*);
 	bool frameheader(int*, int*, double*, double*);
 	bool framebody(int*, double**, double**, double**);
 }
+
+#endif
