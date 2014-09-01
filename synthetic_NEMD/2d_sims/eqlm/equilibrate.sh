6652d74d242b60078c9ae78ad2b13c1b57906ea6
Niall Jackson <niall.jackson@gmail.com>
2014-09-01 18:03:02 +0100

    Added cx1 job script for 2d

--- synthetic_NEMD/2d_sims/eqlm/equilibrate.sh
+++ synthetic_NEMD/2d_sims/eqlm/equilibrate.sh
@@ -4,8 +4,11 @@
 
 EXEC=$HOME/ImperialNESS-Sep14/synthetic_NEMD/bin/sllod_2d.exe
 
+#copy the input file from the directory in which the script was executed to the temporary storage
 cp $PBS_O_WORKDIR/input.dat $TMPDIR
 
+#run the sllod executable
 $EXEC
 
-cp -r $TMPDIR $PBS_O_WORKDIR
+#copy the files generated from the temportary directory back to wherever we started from
+cp -r $TMPDIR $PBS_O_WORKDIR/equilibrated
