8183cba668abcfa8813ec67baecac0c0e4e4e5cc
Niall Jackson <niall.jackson@gmail.com>
2014-09-02 14:07:13 +0100

    Annotations

--- ConfigurationalTemperature/simulation/analyse.sh
+++ ConfigurationalTemperature/simulation/analyse.sh
@@ -7,8 +7,12 @@
 PROGRAM=$HOME/ImperialNESS-Sep14/ConfigurationalTemperature/analysis/C++/analyse 
 #PROGRAM=$HOME/ImperialNESS-Sep14/ConfigurationalTemperature/analysis/FORTRAN/analyse
 
+#copy the trajectory file to the temporary directory
+#(make sure the trajectory file is located in the same folder as this script!)
 cp $PBS_O_WORKDIR/out.lammpstrj.bin $TMPDIR
 
+#execute the C++/FORTRAN analysis code
 $PROGRAM out.lammpstrj.bin
 
+#copy the temperature output back to the initial directory
 mv $TMPDIR/temperatures.dat $PBS_O_WORKDIR/temperatures.dat
